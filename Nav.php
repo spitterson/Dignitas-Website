efd3090824fb8be6f3a2bcbcbad430104bb3e6bd
spitterson <smp@Seans-MBP.home>
2016-01-08 12:47:40 -0500

    website

---
+++ Nav.php
@@ -0,0 +1,12 @@
+ <div class="Nav">
+            <ul>
+            <li><a href="Index.html">Home</a></li>
+            <li><a href="Page2.html">About</a></li>
+            <li><a href="Page3.html">Services</a></li>
+            <li><a href="Page4.html">Companies</a></li>
+            <li><a href="Page5.html">Knowledge Center</a></li>
+            <li><a href="Page6.html">Contact Us</a></li>
+                <li><a href="Page7.html">FAQ</a></li>
+            </ul>
+     <div id="img-wrap"><img src="Imgs/cyber_security.jpg" alt="Security picture" id="small"></div>
+            </div> <!-- end of Nav bar div -->
---
+++ Page1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+        <meta charset="utf-8">
+        <title>Dignitas Site</title>
+        <link rel="stylesheet" href="Css/Main.css">
+        </head>
+    <body>
+        
+        <div class="Container">
+
+<?php include "Nav.php";?>
+            
+            
+            
+            <p>Dignitas Security and Assurance is an advisory platform providing computational, custom solutions for cybersecurity liability insurance among startups and midsized companies in the e-commerce and technology space. We facilitate insurance for technology clients through a proprietary pricing algorithm, a team of the industry's best analysts, and strategic partnership with third-party network security vendors. </p>
